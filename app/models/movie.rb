2c1091d32ac0eb1536df60c708ab80c2be7a4b54
DennisDaddy <softteez95@gmail.com>
2017-02-18 09:49:11 +0300

    add picture validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,13 @@
 class Movie < ApplicationRecord
 	belongs_to :user
 	mount_uploader :picture, PictureUploader
+	validate :picture_size
+
+	# Validates the size of an uploaded picture.
+	def picture_size
+	if picture.size > 5.megabytes
+	errors.add(:picture, "should be less than 5MB")
+	end
+	end
+
 end
